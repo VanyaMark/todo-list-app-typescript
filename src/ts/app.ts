04850f9d0e163bf8ac3270dcf9c8999bd80ecb31
VanyaMark <markova.vanya@gmail.com>
2024-10-03 14:52:07 +0200

    final version before api

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,54 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Get references to the input field, button, and the to-do list container
-  const inputElement = document.getElementById("todo-input") as HTMLInputElement;
-  const addButton = document.getElementById("add-todo-button") as HTMLButtonElement;
+  const inputElement = document.getElementById(
+    "todo-input"
+  ) as HTMLInputElement;
+  const addButton = document.getElementById(
+    "add-todo-button"
+  ) as HTMLButtonElement;
   const todoList = document.getElementById("todo-list") as HTMLUListElement;
-  const inputContainer = document.getElementById("input-container") as HTMLDivElement;
+  const inputContainer = document.getElementById(
+    "input-container"
+  ) as HTMLDivElement;
 
   // 1. Adding new Todo Item on click
-  addButton.addEventListener('click', (event) => {
-    createToDoItem(todoList, inputElement)
-  })
+  addButton.addEventListener("click", () => {
+    createToDoItem(todoList, inputElement);
+  });
 
   // 1. Adding new Todo Item on keyboard Enter
-inputContainer.addEventListener('keydown', (event) => {
-  
-  if (event.key === "Enter") {
-    event.preventDefault();
-    createToDoItem(todoList, inputElement);
-  }
-  // inputElement.value = "";
-})
+  inputContainer.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      // event.preventDefault();
+      createToDoItem(todoList, inputElement);
+    }
+  });
 
   //2. Marking Items as Done
-  todoList.addEventListener('click', (event) => {
+  todoList.addEventListener("click", (event) => {
     const target = event.target as HTMLLIElement;
     // console.log(target.textContent);
 
-    toggleToDoItems(target)
-
-  })
-
+    toggleToDoItems(target);
+  });
 });
 
-// function toggleToDoItems(li: HTMLLIElement) {
-//   if (li.classList.contains('completed')) {
-//     li.classList.remove('completed')
-//   } else {
-//     li.classList.add('completed');
-//   }
-// }
-
 function toggleToDoItems(li: HTMLLIElement) {
-  li.classList.toggle('completed');
+  li.classList.toggle("completed");
 }
 
-function createToDoItem(todoList :HTMLUListElement, inputElement:HTMLInputElement) {
-  const newTodoLiElement = document.createElement('li');
-  newTodoLiElement.textContent = inputElement.value.trim();
-  newTodoLiElement.classList.add('todo-item');
-  todoList.appendChild(newTodoLiElement);
-  inputElement.value = "";
+function createToDoItem(
+  todoList: HTMLUListElement,
+  inputElement: HTMLInputElement
+) {
+  const todoText: string = inputElement.value.trim();
+  if (todoText !== "") {
+    const newTodoLiElement = document.createElement("li");
+    newTodoLiElement.textContent = todoText;
+    newTodoLiElement.classList.add("todo-item");
+    todoList.appendChild(newTodoLiElement);
+  }
+   inputElement.value = "";
 }
 
 // Rana's code
