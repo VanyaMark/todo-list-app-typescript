d3fa02431bcd19ba22d9e82338ba495229dba332
VanyaMark <markova.vanya@gmail.com>
2024-10-03 12:18:17 +0200

    clearing input and trimming it

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,17 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputElement = document.getElementById("todo-input") as HTMLInputElement;
   const addButton = document.getElementById("add-todo-button") as HTMLButtonElement;
   const todoList = document.getElementById("todo-list") as HTMLUListElement;
+  const inputContainer = document.getElementById("input-container") as HTMLDivElement;
 
-  // 1. Adding new Todo Items
-  addButton.addEventListener('click', () => {
-    const newTodoLiElement = document.createElement('li');
-    newTodoLiElement.textContent = inputElement.value;
-    todoList.appendChild(newTodoLiElement)
+  // 1. Adding new Todo Item on click
+  addButton.addEventListener('click', (event) => {
+    createToDoItem(todoList, inputElement)
   })
 
-  //2. Marking Items as Done
+  // 1. Adding new Todo Item on keyboard Enter
+inputContainer.addEventListener('keydown', (event) => {
   
+  if (event.key === "Enter") {
+    event.preventDefault();
+    createToDoItem(todoList, inputElement);
+  }
+  // inputElement.value = "";
+})
 
+  //2. Marking Items as Done
   todoList.addEventListener('click', (event) => {
     const target = event.target as HTMLLIElement;
     // console.log(target.textContent);
@@ -24,10 +31,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+// function toggleToDoItems(li: HTMLLIElement) {
+//   if (li.classList.contains('completed')) {
+//     li.classList.remove('completed')
+//   } else {
+//     li.classList.add('completed');
+//   }
+// }
+
 function toggleToDoItems(li: HTMLLIElement) {
-  if (li.classList.contains('completed')) {
-    li.classList.remove('completed')
-  } else {
-    li.classList.add('completed');
-  }
+  li.classList.toggle('completed');
 }
+
+function createToDoItem(todoList :HTMLUListElement, inputElement:HTMLInputElement) {
+  const newTodoLiElement = document.createElement('li');
+  newTodoLiElement.textContent = inputElement.value.trim();
+  newTodoLiElement.classList.add('todo-item');
+  todoList.appendChild(newTodoLiElement);
+  inputElement.value = "";
+}
+
+// Rana's code
+// function addTodoItem(inputElement:HTMLInputElement, todoList:HTMLUListElement) {
+//   const todoText: string = inputElement.value.trim();
+
+//   if (todoText !== "") {
+//     const newTodoItem = document.createElement("li");
+//     newTodoItem.textContent = todoText;
+//     todoList.appendChild(newTodoItem)
+//   }
+
+//   //reset input field
+//   inputElement.value = "";
+// }
